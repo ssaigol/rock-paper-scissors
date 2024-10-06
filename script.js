dd233865fc94b9b812e1884b76c39af31cf0a4cf
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-06 16:35:13 -0400

    Create functions for Computer & Human choices

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("hello worldd")
+
+function getComputerChoice () {
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+    
+    let a = Math.random();
+    if (a < 0.34) {
+        return rock;
+    } else if (a >= 0.67) {
+        return paper;
+    } else {return scissors}
+}
+
+
+
+
+function getHumanChoice () {
+    for (let i = 0; i < 1;) {
+        let choice = prompt("Please type rock, paper, or scissors");
+        let invalid = "Please enter a valid answer";
+        let valid = `You chose ${choice}.`;
+
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return valid;
+            i++;
+        }
+    }
+}
+
